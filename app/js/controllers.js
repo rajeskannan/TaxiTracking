b18a39cf3da1265432ce900cafb4053a81ac9f34
satishjhanwer <satish.jhanwer@gmail.com>
2014-03-28 17:27:55 +0530

    added dynamic navigation to application and added user registration form.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,23 +2,4 @@
 
 /* Controllers */
 
-/*angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);*/
-
- function CarouselSliderCtrl($scope){
-	var slides = $scope.slides = [];
-
-	slides.push({
-		image: 'img/slide_1.jpg',
-		text: "Call us (800) 1234-5678 Within 24 Hours"
-	});
-	slides.push({
-		image: 'img/slide_2.jpg',
-		text: "We’ll take you Wherever you need"
-	});
-}
+angular.module('myApp.controllers', ["ui.bootstrap"]);
