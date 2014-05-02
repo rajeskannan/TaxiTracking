c9f83e38153ae359907e131d29bd38da5e9fea1d
sayedahammad52 <sayed.a@sigmainfo.net>
2014-05-02 18:10:28 +0530

    some chages in tripactivity service

--- controller/tripTimeTrack_controller.js
+++ controller/tripTimeTrack_controller.js
@@ -15,9 +15,14 @@ function saveStartTripTime(driver_id,status,dispatcher_id,start_time,calfn){
 
 exports.saveStartTripTimeDetails = function(req,res){
 	var driver_id = req.body.driver_id;
-	var status = req.body.status;
+	var status = req.body.statusMessage;
 	var dispatcher_id = req.body.dispatcher_id;
-	var start_time = new Date();
+	var start_time;
+	start_time = req.body.dispatcher_id;
+	if(start_time){
+		console.log("start_time>> "+start_time);
+	}
+	else {start_time = new Date();}
 	res.set('Content-Type', 'application/json');
 	getUserInfo(driver_id,status,dispatcher_id,start_time,function(result){
 		res.write(JSON.stringify(result));
