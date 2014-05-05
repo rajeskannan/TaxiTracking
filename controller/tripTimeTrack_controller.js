70209264cffd540a7dd42e2cd51b4197738d9139
sayedahammad52 <sayed.a@sigmainfo.net>
2014-05-05 11:27:13 +0530

    update trip activity service for end time

--- controller/tripTimeTrack_controller.js
+++ controller/tripTimeTrack_controller.js
@@ -13,21 +13,51 @@ function saveStartTripTime(driver_id,status,dispatcher_id,start_time,calfn){
 		});
  };
 
+
+function saveEndTripTime(id,end_time,calfn){
+	connection.query('UPDATE trip_activity set end_time=? where id=?;' , [id,end_time], function(err, docs) {
+			if(err){console.log('err>> '+err);calfn(null);}
+			else{
+				calfn("true");
+			}
+		});
+ };
+
+
+
+
+
 exports.saveStartTripTimeDetails = function(req,res){
-	var driver_id = req.param("driver_id");
-	var status = req.param("statusMessage");
-	var dispatcher_id = req.param("dispatcher_id");
-	var start_time;
-	start_time = req.param("start_time");
-	if(start_time){
-		console.log("start_time>> "+start_time);
-	}
-	else {start_time = new Date();}
+
 	res.set('Content-Type', 'application/json');
-	saveStartTripTime(driver_id,status,dispatcher_id,start_time,function(result){
-		res.write(JSON.stringify(result));
-		res.end();
-	});
+	var end_time = req.param("end_time");
+	if(end_time){
+		
+		var id = req.param("trip_activity_id");
+		end_time = new Date(end_time);
+		saveEndTripTime(id,end_time,function(result){
+			res.write(JSON.stringify(result));
+			res.end();
+		});
+	}
+	else{
+
+		var driver_id = req.param("driver_id");
+		var status = req.param("statusMessage");
+		var dispatcher_id = req.param("dispatcher_id");
+		var start_time;
+		start_time = req.param("start_time");
+		if(start_time){
+			start_time = new Date(start_time);
+			console.log("start_time>> "+start_time);
+		}
+		else {start_time = new Date();}
+		saveStartTripTime(driver_id,status,dispatcher_id,start_time,function(result){
+			res.write(JSON.stringify(result));
+			res.end();
+		});
+	}
 
 };
+
 
